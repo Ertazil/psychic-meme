621fda7b67e2541200a1e6825010f2a90fb748d1
Ertazil <117348711+Ertazil@users.noreply.github.com>
2024-04-06 11:29:48 +0200

    button css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 export default function Homepage() {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-screen gap-4 px-4">
+        <div className="flex flex-col items-center justify-center w-full h-screen gap-4 px-4" id="patate">
+
             <h1>TRISTAN HOURY</h1>
             <p>Ceci est ma première page</p>
+
+            <div>
+                <h1>test</h1>
+                <button>swap</button>
+            </div>
         </div>
     );
 }
